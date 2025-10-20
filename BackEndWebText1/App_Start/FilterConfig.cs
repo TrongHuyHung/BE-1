0f74cd0403268ac58c01cab2119f026f6f52d4fb
Huy Hung <Huy Hung@LAPTOP-1OD58U1G>
2025-10-20 14:03:43 +0700

    Add project files.

---
+++ BackEndWebText1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace BackEndWebText1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
