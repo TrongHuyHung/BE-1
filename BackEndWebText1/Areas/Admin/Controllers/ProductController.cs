a57209137a2d1098b5d292c5e56c1b623ea9eb18
Huy Hùng <nthuyhung1307@gmail.com>
2025-10-20 23:10:47 +0700

    BE 02

---
+++ BackEndWebText1/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackEndWebText1.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult LocalProduct()
+        {
+            return View();
+        }
+        public ActionResult SortProduct()
+        {
+            return View();
+        }
+        public ActionResult ManageShoppingCart()
+        {
+            return View();
+        }
+        public ActionResult Payments()
+        {
+            return View();
+        }
+        public ActionResult OrderConfirm()
+        {
+            return View();
+        }
+        public ActionResult OrderHistory()
+        {
+            return View();
+        }
+        public ActionResult Account()
+        {
+            return View();
+        }
+    }
+}
